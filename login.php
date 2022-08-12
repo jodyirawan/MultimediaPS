e915c2e6197435dc23e66e4cd61b757fa33117f9
Jody Irawan <89531479+jodyirawan@users.noreply.github.com>
2022-08-12 16:44:27 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include('header.php') ?>
+        <?php 
+        session_start();
+        if(isset($_SESSION['login_id'])){
+            header('Location:home.php');
+        }
+        ?>
+		<title>Login | Simple Online Quiz System</title>
+        <link rel="stylesheet" href="src/style/style.css">
+        <header>
+        <a href="#" class="logo">Web Multimedia Penanggulangan Sampah</a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <ul class="navbar">
+            <li><a href="src/index.html">Home</a></li>
+            <li><a href="src/materi1.html">Materi</a></li>
+        </ul>
+        </header>
+	</head>
+
+	<body id='login-body' class="bg-light">
+
+        <div class="card col-md-6 offset-md-3 text-center mb-4" style="background: rgb(78 175 148)">
+            <h3 class="he3-responsive text-white">Silahkan login untuk mengakses soal</h3>
+        </div>
+		<div class="card col-md-4 offset-md-4 mt-4">
+                <div class="">
+                <h3 class="he3-responsive text-black text-center">Login</h3>
+                </div>
+            <div class="card-body">
+                     <form id="login-frm">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="username" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div> 
+                        <div class="form-group text-right">
+                            <button class="btn btn-primary btn-block" style="background: rgb(78 175 148)" name="submit">Login</button>
+                        </div>
+                        
+                    </form>
+            </div>
+        </div>
+
+		</body>
+
+        <script>
+            $(document).ready(function(){
+                $('#login-frm').submit(function(e){
+                    e.preventDefault()
+                    $('#login-frm button').attr('disable',true)
+                    $('#login-frm button').html('Please wait...')
+
+                    $.ajax({
+                        url:'./login_auth.php',
+                        method:'POST',
+                        data:$(this).serialize(),
+                        error:err=>{
+                            console.log(err)
+                            alert('An error occured');
+                            $('#login-frm button').removeAttr('disable')
+                            $('#login-frm button').html('Login')
+                        },
+                        success:function(resp){
+                            if(resp == 1){
+                                location.replace('home.php')
+                            }else{
+                                alert("Incorrect username or password.")
+                                $('#login-frm button').removeAttr('disable')
+                                $('#login-frm button').html('Login')
+                            }
+                        }
+                    })
+
+                })
+            })
+        </script>
+</html>
